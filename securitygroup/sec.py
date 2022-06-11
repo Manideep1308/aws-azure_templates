8df1478db0bb617393afcba5a081db503c0ea5f7
Manideep1308 <manideep.sankabathula@innoart.in>
2022-06-11 11:33:21 +0530

    added flask-cors in req.txt

--- securitygroup/sec.py
+++ securitygroup/sec.py
@@ -1,10 +1,10 @@
 
 from flask import Flask, request
-
+from flask_cors import CORS
  
  
 app = Flask(__name__)
- 
+CORS(app) 
  
 @app.route('/secgroup', methods=['POST'])
 
--- vmec2/vmec2.py
+++ vmec2/vmec2.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
-
+from flask_cors import CORS
  
  
 app = Flask(__name__)
- 
+CORS(app) 
  
 @app.route('/vm', methods=['POST'])
 
